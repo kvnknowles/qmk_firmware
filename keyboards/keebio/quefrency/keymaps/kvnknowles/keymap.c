dda93ac686d393f952b40e62c25e0da4588486e5
Kevin Knowles <knowles.kevin@gmail.com>
2021-04-26 17:43:51 -0500

    Remove unused encoder code, change caps to esc, move windows and mac mode to w and m, move RGB to keys I'm less than likely to accidently hit

--- keyboards/keebio/quefrency/keymaps/kvnknowles/keymap.c
+++ keyboards/keebio/quefrency/keymaps/kvnknowles/keymap.c
@@ -17,33 +17,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,            KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC, KC_HOME, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,            KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,         KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
     KC_LCTL, KC_LALT, MO(_FN1),KC_LGUI, KC_SPC,           XXXXXXX, KC_SPC,  LCAG_T(KC_RALT), KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65(
     KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,  KC_PSCR, \
-    RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, LAG_SWP, LAG_NRM, _______, _______, _______, \
+    _______, _______, LAG_SWP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, RGB_TOG, \
+    _______, _______, _______, _______, _______, _______, _______, LAG_NRM, _______, _______, _______, _______, _______, RGB_MOD, \
     KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-}
